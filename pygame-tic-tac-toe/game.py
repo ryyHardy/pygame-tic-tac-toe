0a88de8c9652c6027af365067b64dd12911d5e2e
ryyHardy <ryyhardy@gmail.com>
2024-05-10 21:25:11 -0400

    Refactored for better game loop

--- pygame-tic-tac-toe/game.py
+++ pygame-tic-tac-toe/game.py
@@ -1,6 +1,9 @@
 import math
 import pygame as pg
+from typing import Literal
 import sys
+
+pg.init()
 
 # SCREEN
 SCREEN_SIZE = 900
@@ -16,46 +19,61 @@ BG_COLOR = (255, 255, 255)
 
 
 class TicTacToe:
-    def __init__(self) -> None:
+    def __init__(self, starting_player: Literal["X", "O"] = "X") -> None:
         """
-        Initializes tic-tac-toe game and pygame.
+        Initializes tic-tac-toe game.
         """
-        pg.init()
         self.grid = ["" for _ in range(9)]
-        self.turn = "X"
+        self.player = starting_player
 
     def play(self) -> None:
         """
         Starts the game loop and plays the game.
         """
+        # Screen is fixed size. Might add resizable window but it would require significant changes
+        # to drawing code
         self.screen = pg.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
         pg.display.set_caption(SCREEN_TITLE)
         self.draw_grid()
 
+        # GAME LOOP
         while True:
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    sqr = self.get_clicked_square()
+                    if sqr is not None and not self.grid[sqr]:
+                        self.grid[sqr] = self.player
+                        if self.check_win():
+                            print(f"{self.player} WINS!")
+                            return
+                        elif self.check_draw():
+                            print(f"IT'S A DRAW!")
+                            return
+                        self.switch_player()
+            self.draw_grid()
 
-            # This part sucks, but fine
-            grid_index = self.get_mouse_input()
-            if isinstance(grid_index, int):
-                if not self.grid[grid_index]:
-                    self.grid[grid_index] = self.turn
-                    win = self.check_win()
-                    self.draw_grid()
-                    if win:
-                        print(f"{self.turn} WINS!")
-                        return
-                    self.switch_turn()
-            pg.display.update()
+    def get_clicked_square(self) -> int:
+        """
+        Gets the square number clicked by the user.
 
-    def switch_turn(self) -> None:
+        Returns:
+            int | None: Index of the clicked square in the grid array.
+                None if a click does not happen inside the grid.
+        """
+        x, y = pg.mouse.get_pos()
+        col, row = math.floor(x / SQR_SIZE), math.floor(y / SQR_SIZE)
+        i = row * 3 + col
+        if i in range(9):
+            return i
+
+    def switch_player(self) -> None:
         """
         Flips the turn of the game to the other player.
         """
-        self.turn = "O" if self.turn == "X" else "X"
+        self.player = "O" if self.player == "X" else "X"
 
     def check_win(self) -> bool:
         """
@@ -66,7 +84,7 @@ class TicTacToe:
         """
         # https://www.reddit.com/r/learnpython/comments/v8bscr/comment/ibs9e6y/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
         return any(
-            self.turn == self.grid[a] == self.grid[b] == self.grid[c]
+            self.player == self.grid[a] == self.grid[b] == self.grid[c]
             for a, b, c in [
                 (0, 1, 2),
                 (3, 4, 5),
@@ -79,20 +97,14 @@ class TicTacToe:
             ]
         )
 
-    def get_mouse_input(self) -> int | None:
+    def check_draw(self) -> bool:
         """
-        Gets the square number clicked by the user.
+        Determines if the game has reached a draw.
 
         Returns:
-            int | None: Index of the clicked square in the grid array.
-                None if a click does not happen inside the grid.
+            bool: True if it is draw (i.e. board is full). False otherwise.
         """
-        if pg.mouse.get_pressed()[0]:
-            x, y = pg.mouse.get_pos()
-            col, row = math.floor(x / SQR_SIZE), math.floor(y / SQR_SIZE)
-            i = row * 3 + col
-            if i in range(9):
-                return i
+        return all(self.grid) and not self.check_win()
 
     def draw_grid(self) -> None:
         """
@@ -110,3 +122,4 @@ class TicTacToe:
                     pg.draw.rect(self.screen, sqr_color, sqr_rect)
 
                 pg.draw.rect(self.screen, GRID_COLOR, sqr_rect, width=3)
+        pg.display.update()
